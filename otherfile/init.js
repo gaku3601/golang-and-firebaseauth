451c0633ba227868dc1b48cd03b557c0d46b4bfd
日高岳大 <gaku@gakumbp.local>
2019-10-13 08:41:50 +0900

    認証ページの作成

---
+++ otherfile/init.js
@@ -0,0 +1,10 @@
+var config = {
+    apiKey: "AIzaSyDZ8-YbwATg8juWPluPhUteJME5WqzVGxo",
+    authDomain: "timevalue-ccdfc.firebaseapp.com",
+    databaseURL: "https://timevalue-ccdfc.firebaseio.com",
+    projectId: "timevalue-ccdfc",
+    storageBucket: "timevalue-ccdfc.appspot.com",
+    messagingSenderId: "329398769215",
+    appId: "1:329398769215:web:8973b7109a43c273d98a8a"
+};
+firebase.initializeApp(config);
